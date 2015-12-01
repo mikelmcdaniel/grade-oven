25d7296aaf0bada0f8eac480a8d65f849a61cdb4
Mikel Mcdaniel <mikelmcdaniel@gmail.com>
2015-12-01 00:08:40 -0800

    Build datastore.py on top of LevelDB so it is more robust, except that keys are now restricted.

--- src/datastore.py
+++ src/datastore.py
@@ -1,65 +1,90 @@
-import json
+import cStringIO
 import os
 import shutil
+import tempfile
+
+import leveldb
 
 
 class DataStore(object):
-  def __init__(self, dir_path):
-    self.dir_path = dir_path
-    self._data = {}
+  def __init__(self, dir_path=None):
+    if dir_path is None:
+      dir_path = tempfile.mkdtemp()
+    self.db = leveldb.LevelDB(dir_path, paranoid_checks=True)
 
-  def load(self):
-    with open(os.path.join(self.dir_path, 'db.json')) as f:
-      self._data = json.load(f)
-
-  def save(self):
-    with open(os.path.join(self.dir_path, 'db_new.json'), 'w') as f:
-      json.dump(self._data, f)
-    shutil.move('db_new.json', 'db.json')
 
   def put(self, key, value):
-    sub_tree = value
-    cur = self._data
-    for j, key_part in enumerate(key):
-      if key_part not in cur:
-        for k in xrange(len(key) - 1, j, -1):
-          sub_tree = {key[k]: sub_tree}
-        break
-      else:
-        cur = cur[key_part]
-    cur[key_part] = sub_tree
+    mods = leveldb.WriteBatch()
+    partial_key = cStringIO.StringIO()
+    for j, key_part in enumerate(key, 1):
+      partial_key.write('\x00')
+      len_partial = partial_key.tell()
+      partial_key.seek(0)
+      partial_key.write(chr(j))
+      partial_key.seek(len_partial)
+      partial_key.write(key_part)
+      partial_key.seek(0)
+      mods.Put(partial_key.read(), '')
+    partial_key.seek(0)
+    mods.Put(partial_key.read(), value)
+    self.db.Write(mods)
 
   def get(self, key):
-    cur = self._data
-    try:
-      for sub_key in key:
-        cur = cur[sub_key]
-      return cur
-    except KeyError:
-      return None
+    real_key = cStringIO.StringIO()
+    real_key.write(chr(len(key)))
+    real_key.write('\x00'.join(key))
+    real_key.seek(0)
+    return self.db.Get(real_key.read())
 
   def get_all(self, key):
-    return tuple(self.get(key))
+    real_key = cStringIO.StringIO()
+    real_key.write(chr(len(key) + 1))
+    real_key.write('\x00'.join(key))
+    real_key.seek(0)
+    start_key = real_key.read()
+    real_key.write('\x01')
+    real_key.seek(0)
+    end_key = real_key.read()
+    sub_keys = []
+    for sub_key in self.db.RangeIter(start_key, end_key, include_value=False):
+      sub_keys.append(sub_key.rsplit('\x00')[-1])
+    return sub_keys
 
   def remove(self, key):
-    cur = self._data
-    for sub_key in key[:-1:]:
-      cur = cur[sub_key]
-    del cur[key[-1]]
+    real_key = cStringIO.StringIO()
+    real_key.write(chr(len(key)))
+    real_key.write('\x00'.join(key))
+    real_key.seek(0)
+    mods = leveldb.WriteBatch()
+    for j in xrange(len(key), 256):
+      real_key.seek(0)
+      real_key.write(chr(j))
+      real_key.seek(0)
+      start_key = real_key.read()
+      end_key = start_key + '\x01'
+      found_keys = False
+      for k in self.db.RangeIter(start_key, end_key, include_value=False):
+        mods.Delete(k)
+        found_keys = True
+      if not found_keys:
+        break
+    self.db.Write(mods)
 
 
 if __name__ == '__main__':
   key1 = ('courses', 'c++',    'assignments', 'homework 1')
   key2 = ('courses', 'c++',    'assignments', 'homework 2')
   key3 = ('courses', 'python', 'assignments', 'homework 1')
-  store = DataStore(os.getcwd())
-  store.put(key1, 'c++ 1')
-  store.put(key2, 'c++ 2')
-  store.put(key3, 'python 1')
-  assert set(store.get_all(('courses',))) == set(['c++', 'python'])
-  assert store.get(key1) == 'c++ 1'
-  assert store.get(key3) == 'python 1'
-  store.remove(('courses', 'c++'))
-  assert set(store.get_all(('courses',))) == set(['python'])
-
-
+  temp_dir = tempfile.mkdtemp()
+  try:
+    store = DataStore(temp_dir)
+    store.put(key1, 'c++ 1')
+    store.put(key2, 'c++ 2')
+    store.put(key3, 'python 1')
+    assert set(store.get_all(('courses',))) == set(['c++', 'python'])
+    assert store.get(key1) == 'c++ 1'
+    assert store.get(key3) == 'python 1'
+    store.remove(('courses', 'c++'))
+    assert set(store.get_all(('courses',))) == set(['python'])
+  finally:
+    shutil.rmtree(temp_dir)
