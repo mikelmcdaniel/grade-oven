49122b4a1ae905f49ba33623ed66f13447e7b46f
Mikel Mcdaniel <mikelmcdaniel@gmail.com>
2015-12-21 01:02:36 -0800

    Add apt-get installs to setup.sh

--- setup/setup.sh
+++ setup/setup.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -e  # exit on error
 
-sudo echo  # Used to get and cache sudo password quickly
 docker build -t grade_oven/grade_oven -f grade_oven.Dockerfile --rm --memory-swap=-1 .
+
+sudo apt-get --assume-yes install python2.7 python-flask python-flask-login docker python-bcrypt python-leveldb
+
 ./mount.sh
