a4eaa6890357175b52d340037e1fb8e500fc9fb5
Mikel Mcdaniel <mikelmcdaniel@gmail.com>
2018-10-04 00:15:04 -0700

    Update maximum number of inodes in host_dirs from 50 to 1000.

--- setup/mount.sh
+++ setup/mount.sh
@@ -2,6 +2,6 @@
 for d in ../data/host_dirs/{0,1,2,3}; do
   mkdir -p "$d"
   sudo umount -t tmpfs "$d";
-  sudo mount -t tmpfs -o size=64m,nr_inodes=50,mode=0700,nodev,rw tmpfs "$d";
+  sudo mount -t tmpfs -o size=64m,nr_inodes=1000,mode=0700,nodev,rw tmpfs "$d";
   sudo chown ${USER}:${USER} "$d"
 done
